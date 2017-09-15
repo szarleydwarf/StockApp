956b324cea3214b84df5dad7042f15f4278b853c
Szarley <szarleydwarf@gmail.com>
2017-09-15 17:01:51 +0100

    Revert "14/9/17"
    This reverts commit 3fa68d2f8b75463aec77283db400203032bc270c.

--- src/utillity/StockPrinter.java
+++ src/utillity/StockPrinter.java
@@ -1,12 +1,12 @@
 package utillity;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.print.PageFormat;
 import java.awt.print.Printable;
 import java.awt.print.PrinterException;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Calendar;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
@@ -17,44 +17,15 @@ import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 
 public class StockPrinter implements Printable {
 	private final String savePath = "D:/@Development/__TEMP/", imagePath = "D:/@Development/EclipseJavaProjects/sqliteTestApp/StockApp/resources/img/Logo HCT 245x84.png";
-	private String docName = "lol1.pdf";
+	private String docName = "lol.pdf";
 	private String docPath = "";
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 	private PDPageContentStream contentStream ;
 	
-	private ArrayList<ArrayList<String>> servicesList;
 	
 	public StockPrinter(){
 		
 	}
-=======
->>>>>>> parent of 3fa68d2... 14/9/17
-=======
->>>>>>> parent of 3fa68d2... 14/9/17
-=======
->>>>>>> parent of 3fa68d2... 14/9/17
->>>>>>> lol
-=======
->>>>>>> parent of 3fa68d2... 14/9/17
-=======
->>>>>>> parent of 3fa68d2... 14/9/17
-=======
->>>>>>> parent of 3fa68d2... 14/9/17
-=======
->>>>>>> parent of 3fa68d2... 14/9/17
 	
-	public StockPrinter(ArrayList<ArrayList<String>> servicesList) {
-		this.servicesList = servicesList;
-	}
-
 	public void printDoc() throws IOException{
 //		System.out.println("Printing");
 		generatePDF();
@@ -64,17 +35,8 @@ public class StockPrinter implements Printable {
 		PDDocument doc = new PDDocument();
 		PDPage page = new PDPage();
 		doc.addPage(page);
-		PDPageContentStream contentStream = new PDPageContentStream(doc, page);
+		contentStream = new PDPageContentStream(doc, page);
 		PDImageXObject pdImage = PDImageXObject.createFromFile(imagePath, doc);
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 				
 		contentStream.drawImage(pdImage, 215,  675);
 		contentStream.setNonStrokingColor(Color.GRAY);
@@ -95,12 +57,8 @@ public class StockPrinter implements Printable {
 		contentStream.setNonStrokingColor(Color.BLACK);
 		contentStream.setFont(PDType1Font.COURIER, 18);
 		
-		for(int i = 0; i < this.servicesList.size(); i++){
-			ArrayList<String> tList = this.servicesList.get(i);
-			contentStream.showText((i+1)+"    ");
-			for(int j = 0; j < tList.size(); j++){
-				contentStream.showText(tList.get(j)+"    ");
-			}
+		for(int i = 0; i < 5; i++){
+			contentStream.showText((i+1)+"       CarWash              1              €7");
 			contentStream.newLine();			
 		}
 		
@@ -111,32 +69,26 @@ public class StockPrinter implements Printable {
 		//table of services/products done
 	
 		contentStream.close();
-=======
->>>>>>> parent of 3fa68d2... 14/9/17
-=======
->>>>>>> parent of 3fa68d2... 14/9/17
-=======
->>>>>>> parent of 3fa68d2... 14/9/17
->>>>>>> lol
-=======
->>>>>>> parent of 3fa68d2... 14/9/17
-=======
->>>>>>> parent of 3fa68d2... 14/9/17
-=======
->>>>>>> parent of 3fa68d2... 14/9/17
-=======
->>>>>>> parent of 3fa68d2... 14/9/17
 		
+		
+		//TODO:
+		// docName change to the date with customer name and number eg today/total
+		docPath = savePath+docName;
+		doc.save(docPath);
+		doc.close();
+	}
+
+	private void fillCompanyDetails() throws IOException {
 		Calendar today = Calendar.getInstance();
 		SimpleDateFormat df= new SimpleDateFormat("dd/MM/yyyy");
 		
 		String date = df.format(today.getTime());
-		System.out.println(page.getArtBox());
+
 		
-		contentStream.drawImage(pdImage, 200,  675);
+		contentStream.setNonStrokingColor(Color.BLACK);
 		contentStream.beginText();
 		contentStream.setFont(PDType1Font.COURIER, 18);
-		contentStream.newLineAtOffset(7.5f,  740);
+		contentStream.newLineAtOffset(25f,  740);
 		contentStream.setLeading(20.5f);
 		
 		String text = "HCT",
@@ -165,16 +117,6 @@ public class StockPrinter implements Printable {
 		contentStream.showText(text8);
 		
 		contentStream.endText();
-
-	
-		contentStream.close();
-		
-		
-		//TODO:
-		// docName change to the date with customer name and number eg today/total
-		docPath = savePath+docName;
-		doc.save(docPath);
-		doc.close();
 	}
 
 	@Override
