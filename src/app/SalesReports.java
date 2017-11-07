c0f3012929f1b4e038460c1da4ebd92483bc547c
Szarley <szarleydwarf@gmail.com>
2017-11-07 11:53:54 +0000

    07/11/2017
    Sales Report screen ready
    Is not hundretprocent accurate though. The issue - doesn't take in account  qnt of the services/items sold

--- src/app/SalesReports.java
+++ src/app/SalesReports.java
@@ -17,6 +17,7 @@ import javax.swing.JScrollPane;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
 
 import javax.swing.SwingConstants;
 import javax.swing.border.Border;
@@ -79,13 +80,9 @@ public class SalesReports {
 		
 		String q1 = "SELECT "+this.fv.SERVICE_TABLE_NUMBER+","+this.fv.COST+" FROM "+ this.fv.SERVICES_TABLE;
 		this.servicesCosts = (HashMap<String, Double>) this.DM.getAllCostsPrices(q1);
-//		printMap(servicesCosts);
-		System.out.println("\n\n!");
+		
 		String q2 = "SELECT "+this.fv.STOCK_TABLE_NUMBER+","+this.fv.COST+" FROM "+ this.fv.STOCK_TABLE;
 		this.stocksCosts = (HashMap<String, Double>) this.DM.getAllCostsPrices(q2);
-//		printMap(stocksCosts);
-	
-		
 		
 		initialize();
 	}
@@ -167,32 +164,44 @@ public class SalesReports {
 	}
 
 	private void populateTable() {
+		DecimalFormat df;
+		df = new DecimalFormat(this.fv.DECIMAL_FORMAT); 
 		String query = "";
-		Object data[][] = {};
-		for(int j = this.fv.MONTHS_2017.length-1; j >= 0; j--) {
+		String[][] data = new String [this.fv.MONTHS_2017.length][this.fv.SALES_REPORT_TB_HEADINGS.length];
+		for(int j = 0; j < this.fv.MONTHS_2017.length; j++) {
 			query = "SELECT "+this.fv.SERVICE_TABLE_NUMBER+","+this.fv.STOCK_TABLE_NUMBER+","+this.fv.TOTAL+" FROM "+this.fv.INVOCE_TABLE+" WHERE "+this.fv.INVOCE_TABLE_DATE+" LIKE '%"+this.fv.MONTHS_2017[j]+"%'";
 			ArrayList<String> list = new ArrayList<String>();
 			list = this.DM.selectRecordArrayList(query);
-			
-			data[0][0] = this.fv.MONTHS_2017[0];
-			
-			if(!list.isEmpty()) {
+			data[j][0] = this.fv.MONTHS_2017[j];
+			if(list.isEmpty()){
+				data[j][1] = ""+0;
+				data[j][2] = ""+0;
+				data[j][3] = ""+0;			
+			} else {
+				double dCost = 0;
+				double  incomeSum = 0;
 				for(int i = 0; i < list.size(); i++) {
-					if(!list.get(i).equals("") && ((i != 2) && (i != 5))){
+					if(!list.get(i).equals("") && ((i != 2) && (i != 5) && (i != 8) && (i != 11))) {
 						String[] tokens = list.get(i).split(",", -1);
-						
-						double monthlyCosts = sumCosts(tokens);
-						System.out.println(i + " monthlySum " + monthlyCosts);
-						data[0][1] = monthlyCosts;
+						dCost  += sumCosts(tokens);
+					}else if(!list.get(i).equals("")){
+						incomeSum += Double.parseDouble(list.get(i));		
 					}
 				}
+				data[j][1] = "€ "+ df.format(dCost);
+				data[j][2] = "€ "+ df.format(incomeSum);
+				double dDiff = incomeSum - dCost;
+				data[j][3] = "€ "+ df.format(dDiff);
 			}
+			
 		}
 		table = new JTable(data, this.fv.SALES_REPORT_TB_HEADINGS);
 		table.setBounds(42, 87, 560, 288);
 		table.setPreferredScrollableViewportSize(new Dimension(500, 150));
 		table.setFillsViewportHeight(true);
 		JScrollPane scrollPane = new JScrollPane(table);
+		scrollPane.setLocation(42, 95);
+		scrollPane.setSize(560, 240);
 		
 		frame.getContentPane().add(scrollPane);
 		
@@ -201,7 +210,6 @@ public class SalesReports {
 	private double sumCosts(String[] tokens) {
 		double sum = 0;
 		for (String token : tokens) {
-			System.out.println("token " + token);
 			double d = 0;
 			if(this.servicesCosts.containsKey(token))
 				d = this.servicesCosts.get(token);
@@ -210,6 +218,6 @@ public class SalesReports {
 			
 			sum += d;
 		}
-		return sum;
+		return (sum);
 	}
 }
